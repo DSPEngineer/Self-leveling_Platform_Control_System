8754d20672fbaeb1a41482ba160b235952f5439d
Jose Pagan <jose.pagan@terex.com>
2025-04-17 20:28:15 -0700

    Using new brancg for changes

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,10 @@ void setup()
   Serial.print( " DONE: BEE-526 - Self-leveling Platform Control System\n" );    
 }
 
+int count=0;
 void loop()
 {  // put your main code here, to run repeatedly:
-  Serial1.print( "LOOP: BEE-526 - Self-leveling Platform Control System\n" );
+  Serial1.printf( "LOOP: BEE-526 - Self-leveling Platform Control System - %d\n", count++ );
 }
 
 // put function definitions here:
